eeb746ed33b51141b5a5f533b342b26f272ab2e4
Inthava XAIYALOUN <coci8750@gmail.com>
2024-09-14 19:57:05 +0700

    add hello

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "world"
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("World"))
+}
