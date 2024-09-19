d263b0d6de52f288b419817456ab0aa3c35b70ea
Inthava XAIYALOUN <coci8750@gmail.com>
2024-09-20 04:44:21 +0700

    shape

---
+++ structs/shape_test.go
@@ -0,0 +1,33 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	ractangle := Rectangle{10.0, 10.0}
+	got := ractangle.Perimeter()
+	want := 40.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 60.0}
+		want := 72.0
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+}
