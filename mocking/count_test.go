d7921dc3fd3ae78a321276f89e12f8ff55302e3d
Inthava <coci8750@gmail.com>
2024-12-12 06:08:10 +0700

    mocking:n add coutdown

---
+++ mocking/count_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCount(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+	got := buffer.String()
+	want := "3"
+	if got != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
