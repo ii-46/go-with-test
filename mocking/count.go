ddd8d6215249f4a07e46140ec95a8d55ce1201f9
Inthava <coci8750@gmail.com>
2024-12-12 06:26:04 +0700

    mocking dependency

--- mocking/count.go
+++ mocking/count.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
+	"time"
 )
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
 
 const finalWord = "Go!"
 const countDownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countDownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- mocking/count_test.go
+++ mocking/count_test.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
+const sleep = "sleep"
+const write = "write"
+
+type SpyCountDownOperation struct {
+	Calls []string
+}
+
+func (s *SpyCountDownOperation) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountDownOperation) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpySleeper struct {
+	Call int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Call++
+}
+
 func TestCount(t *testing.T) {
 	buffer := &bytes.Buffer{}
-
-	Countdown(buffer)
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
 	got := buffer.String()
 	want := `3
 2
@@ -17,4 +42,24 @@ Go!`
 	if got != want {
 		t.Errorf("want %q, got %q", want, got)
 	}
+	if spySleeper.Call != 4 {
+		t.Errorf("want 4 got %d", spySleeper.Call)
+	}
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountDownOperation{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter)
+		}
+	})
 }
