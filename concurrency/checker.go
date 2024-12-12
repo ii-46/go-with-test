26fc75cc3cbfdb7106822efc3d289564bbabc394
Inthava <coci8750@gmail.com>
2024-12-12 08:47:03 +0700

    using channel

--- concurrency/checker.go
+++ concurrency/checker.go
@@ -2,10 +2,22 @@ package main
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChanal := make(chan result)
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(s string) {
+			resultsChanal <- result{s, wc(s)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		r := <-resultsChanal
+		results[r.string] = r.bool
 	}
 	return results
 }
