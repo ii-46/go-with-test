231f1a9f7b709990b9ac8e1d7a3ad14bd1a193c0
Inthava <coci8750@gmail.com>
2024-12-26 11:27:29 +0700

    use select to recive chan

--- concurrency/racer_test.go
+++ concurrency/racer_test.go
@@ -17,10 +17,20 @@ func TestRacer(t *testing.T) {
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
 	want := fastURL
-	got := Racer(slowURL, fastURL)
+	got, _ := Racer(slowURL, fastURL)
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayServer(11 * time.Second)
+		serverB := makeDelayServer(12 * time.Second)
+		defer serverA.Close()
+		defer serverB.Close()
+		_, err := Racer(serverA.URL, serverB.URL)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 
 }
 
